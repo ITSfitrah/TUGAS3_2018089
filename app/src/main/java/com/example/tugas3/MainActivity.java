f4b1bb2d740a0deea936e6ed54f174f10c4c995b
ITSfitrah <alpitrah125@gmail.com>
2022-11-08 01:52:13 +0700

    TUGAS5_2018089

--- app/src/main/java/com/example/tugas3/MainActivity.java
+++ app/src/main/java/com/example/tugas3/MainActivity.java
@@ -5,14 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
-
+    RecyclerView recylerView;
+    String s1[], s2[],s3[];
+    int images[] =
+            {R.drawable.gambar1,R.drawable.gambar2,R.drawable.gambar3,R.drawable.gambar4};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recylerView = findViewById(R.id.recyclerView);
+        s1 = getResources().getStringArray(R.array.makanan);
+        s2 = getResources().getStringArray(R.array.deskripsi);
+        s3 = getResources().getStringArray(R.array.star);
+        destiadapter appAdapter = new
+                destiadapter(this,s1,s2,s3,images);
+        recylerView.setAdapter(appAdapter);
+        LinearLayoutManager layoutManager = new LinearLayoutManager((this), RecyclerView.HORIZONTAL, false);
+        recylerView.setLayoutManager(layoutManager);
     }
+
+
+
     public void MainActivity2(View view){
         Intent a = new Intent(MainActivity.this,
                 MainActivity2.class);
@@ -28,4 +45,7 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity4.class);
         startActivity(a);
     }
+
+
+
 }
